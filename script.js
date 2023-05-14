02e8f61c4ca824663f4bffa59a849059358fb14c
Braulio2002 <107601988+Braulio2002@users.noreply.github.com>
2023-05-14 02:17:27 -0500

    initial commit

---
+++ script.js
@@ -0,0 +1,10 @@
+const surpriseButton = document.getElementById('surprise-button');
+const surpriseElement = document.getElementById('surprise');
+
+surpriseButton.addEventListener('click', () => {
+  surpriseElement.style.display = 'block';
+  window.alert('Te quiero, mamá!');
+  window.alert('Gracias');
+  window.alert('Por tenernos');
+  window.alert('y por luchar por nosotros');
+});
